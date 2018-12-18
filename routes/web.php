146a0048781c182ff57098c4eba586d6de10399a
3a532043 <s3a532043@student.ncut.edu.tw>
2018-12-18 21:24:48 +0800

    練習九：將路由換成控制器

--- routes/web.php
+++ routes/web.php
@@ -18,16 +18,12 @@ Route::group(['prefix'=>'student'],function ()
 
     Route::get('{student_no}',[
         'as'=>'student',
-        'uses'=>function ($student_no) {
-            return '學號：'.$student_no;
-        }
+                'uses'=>'StudentController@getStudentData'
     ]);
 
 
     Route::get('{student_no}/score/{subject?}', [
     'as'=>'student.score',
-    'uses'=>function ($student_no,$subject = null) {
-        return '學號：'.$student_no.'的'.((is_null($subject))?'所有科目':$subject).'成績';
-    }
+        'uses'=>'StudentController@getStudentScore'
     ])->where(['subject'=>'(chinese|english|math)']);
 });
