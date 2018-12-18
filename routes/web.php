63032a7db0e4c940518bcf673b7f95fa37207a7e
3a532043 <s3a532043@student.ncut.edu.tw>
2018-12-18 21:20:41 +0800

    練習七：路由命名

--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,21 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::pattern('student_no','s[0-9]{10}');
-Route::group(['prefix'=>'student'],function (){
-    Route::get('student/{student_no}', function ($student_no) {
-        return "學號：".$student_no;
-});
+Route::group(['prefix'=>'student'],function ()
+{
 
-Route::get('{student_no}/score/{subject?}', function ($student_no,$subject = null) {
-    return "學號：".$student_no."的".((is_null($subject))?"所有科目":$subject)."成績";
-})->where(['subject'=>'(chinese|english|math)']);
+    Route::get('{student_no}',[
+        'as'=>'student',
+        'uses'=>function ($student_no) {
+            return '學號：'.$student_no;
+        }
+    ]);
+
+
+    Route::get('{student_no}/score/{subject?}', [
+    'as'=>'student.score',
+    'uses'=>function ($student_no,$subject = null) {
+        return '學號：'.$student_no.'的'.((is_null($subject))?'所有科目':$subject).'成績';
+    }
+    ])->where(['subject'=>'(chinese|english|math)']);
 });
